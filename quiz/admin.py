ae6d934266d1850729f6abfd90b177a24f780733
khauhelo <khauhelo872@gmail.com>
2026-05-13 15:58:35 +0200

    Remove unused import of TranslationModelForm from admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
-from modeltranslation.forms import TranslationModelForm
 
 from .models import (
     Quiz,
