df614224717e364094a06c26639bb71d80aeb89d
khauhelo <khauhelo872@gmail.com>
2026-05-24 00:10:00 +0200

    Remove merge conflict markers and clean up UserAdmin registration in admin.py; update label for username in login.html

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import User, Student, Parent, TeacherProfile
-from .models import User, Student, Parent
+
 
 
 class UserAdmin(admin.ModelAdmin):
--- accounts/models.py
+++ accounts/models.py
@@ -70,17 +70,18 @@ class User(AbstractUser):
         blank=True,
         related_name="users",
     )
-    is_student = models.BooleanField(default=False)
 
-class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_lecturer = models.BooleanField(default=False)
     is_parent = models.BooleanField(default=False)
+
     gender = models.CharField(max_length=1, choices=GENDERS, blank=True, null=True)
     phone = models.CharField(max_length=60, blank=True, null=True)
     address = models.CharField(max_length=60, blank=True, null=True)
     picture = models.ImageField(
-        upload_to="profile_pictures/%y/%m/%d/", default="default.png", null=True
+        upload_to="profile_pictures/%y/%m/%d/",
+        default="default.png",
+        null=True,
     )
     email = models.EmailField(blank=True, null=True)
 
@@ -90,58 +91,6 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ("-date_joined",)
-
-    @property
-    def get_full_name(self):
-        full_name = self.username
-        if self.first_name and self.last_name:
-            full_name = self.first_name + " " + self.last_name
-        return full_name
-
-    def __str__(self):
-        return "{} ({})".format(self.username, self.get_full_name)
-
-    @property
-    def get_user_role(self):
-        if self.is_superuser and self.school:
-            role = _("Principal")
-        elif self.is_superuser:
-            role = _("Platform Admin")
-        elif self.is_student:
-            role = _("Student")
-        elif self.is_lecturer:
-            role = _("Lecturer")
-        elif self.is_parent:
-            role = _("Parent")
-
-        return role
-
-    def get_picture(self):
-        try:
-            return self.picture.url
-        except:
-            no_picture = settings.MEDIA_URL + "default.png"
-            return no_picture
-
-    def get_absolute_url(self):
-        return reverse("profile_single", kwargs={"user_id": self.id})
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        try:
-            img = Image.open(self.picture.path)
-            if img.height > 300 or img.width > 300:
-                output_size = (300, 300)
-                img.thumbnail(output_size)
-                img.save(self.picture.path)
-        except:
-            pass
-
-    def delete(self, *args, **kwargs):
-        if self.picture.url != settings.MEDIA_URL + "default.png":
-            self.picture.delete()
-        super().delete(*args, **kwargs)
-
 
 class StudentManager(models.Manager):
     def search(self, query=None):
